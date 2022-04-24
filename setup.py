b07c892fdcf0eb19d240f9a7ddb0af6be2b3733e
TotalJTM <jermcg22@gmail.com>
2022-04-24 09:28:14 -0400

    restructure

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import os
 from glob import glob
 from setuptools import setup
 
+
 package_name = 'P3DX_robot'
 
 setup(
     name=package_name,
     version='0.0.0',
+    scripts=['p3dx_robot_controller.py','serial_comm_ros.py'],
     # Packages to export
     packages=[package_name],
     # Files we want to install, specifically launch files
@@ -16,7 +18,8 @@ setup(
         # Include our package.xml file
         (os.path.join('share', package_name), ['package.xml']),
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        #(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/*.launch.py'))),
     ],
     # This is important as well
     install_requires=['setuptools'],
